dbc71fe318f2fd6502b4c603443e797c25cfbe7b
yangjingzheng <1318911846@qq.com>
2018-02-12 11:56:44 +0800

    keyword

---
+++ src/views/node/case/index.vue
@@ -0,0 +1,186 @@
+<template>
+    <card>
+  <div>
+    <div class="top">
+      案例中心:
+      <Select v-model="industry_id" clearable label-in-value
+              style="width:150px;text-align: left" placeholder="请选择行业分类">
+        <Option v-for="(item, index) in industry" :value="item.id" :label="item.name" :key="index">
+          {{ item.name }}
+        </Option>
+      </Select>
+      <Input v-model="keyword" placeholder="案例关键词" style="width: 300px"></Input>
+      <Input v-model="content" placeholder="全文检索" style="width: 300px"></Input>
+      <Button type="primary" @click="queryData">查询</Button>
+    </div>
+    <div class="content" style="margin-top:10px;">
+      <div style="width:90%;margin: 0px auto">
+        <Row>
+          <Col span="6" v-for="(item,index) in datas" :key="index">
+          <div class="siteborder" style="cursor: pointer" @click="show(index)">
+            <div class="sitewidth" :style=choose_bgimg(index)>
+              <div class="caseclass">
+                <p style="font-size:18px" :title=item.title>{{item.title}}</p>
+                <p style="font-size: 15px" title="关键词">{{item.keyword}}</p>
+              </div>
+            </div>
+            <div class="sitebottom">
+              <Icon type="ios-navigate-outline"></Icon>
+              <span style="font-size: 13px;display: inline-block;margin-right: 15px"> {{item.industry_name}}</span>
+              <Icon type="ios-paperplane"></Icon>
+              <span style="font-size: 13px">{{item.source}}</span>
+            </div>
+          </div>
+          </Col>
+        </Row>
+      </div>
+      <br>
+    </div>
+    <div class="content" style="margin-top:10px;">
+      <div style="margin: 10px;overflow: hidden">
+        <div style="float: right;">
+          <Page :total="total" :current="current" @on-change="changePage" @on-page-size-change="changePageSize"
+                show-total
+                show-elevator></Page>
+        </div>
+      </div>
+    </div>
+    <caseshow ref="show" :form="editinfo"></caseshow>
+  </div>
+    </card>
+</template>
+<script type="text/ecmascript-6">
+  import http from '../../../libs/http';
+  import caseshow from './show.vue';
+  import common from '../../../libs/common';
+
+  export default {
+      data () {
+          return {
+              self: this,
+              border: true,
+              stripe: true,
+              showheader: true,
+              showIndex: true,
+              size: 'small',
+              current: 1,
+              total: 0,
+              page: 1,
+              rows: 8,
+              title: '',
+              datas: [],
+              editinfo: {},
+              industry: [],
+              industry_id: '',
+              keyword: '',
+              content: ''
+          };
+      },
+      components: {caseshow},
+      created () {
+          this.getData();
+          this.getIndustry();
+          this.backgroundcolor();
+      },
+      methods: {
+          choose_bgimg (index) {
+              let background = this.$store.state.commondata.backgroundcolor;
+              index = index % background.length;
+              return background[index];
+          },
+          getData () {
+              let data = {
+                  params: {
+                      page: this.page,
+                      rows: this.rows,
+                      industry_id: this.industry_id,
+                      keyword: this.keyword,
+                      content: this.content
+                  }
+              };
+              this.apiGet('case_center', data).then((data) => {
+                  this.handleAjaxResponse(data, (data, msg) => {
+                      this.datas = data.rows;
+                      this.total = data.total;
+                  }, (data, msg) => {
+                      this.$Message.error(msg);
+                  });
+              }, (data) => {
+                  this.$Message.error('网络异常，请稍后重试');
+              });
+          },
+          getIndustry () {
+              this.apiGet('industries').then((res) => {
+                  this.handleAjaxResponse(res, (data, msg) => {
+                      this.industry = data;
+                  }, (data, msg) => {
+                      this.$Message.error(msg);
+                  });
+              }, (res) => {
+                  // 处理错误信息
+                  this.$Message.error('网络异常，请稍后重试。');
+              });
+          },
+          show (index) {
+              let editid = this.datas[index].id;
+              this.apiGet('case_center/' + editid).then((res) => {
+                  this.handleAjaxResponse(res, (data, msg) => {
+                      this.editinfo = data;
+                      this.modal = false;
+                      this.$refs.show.modal = true;
+                  }, (data, msg) => {
+                      this.$Message.error(msg);
+                  });
+              }, (res) => {
+                  // 处理错误信息
+                  this.$Message.error('网络异常，请稍后重试。');
+              });
+          },
+          changePage (page) {
+              this.page = page;
+              this.getData();
+          },
+          changePageSize (pagesize) {
+              this.rows = pagesize;
+              this.getData();
+          },
+          queryData () {
+              this.getData();
+          }
+      },
+      computed: {},
+      mixins: [http, common]
+  };
+</script>
+<style>
+  .siteborder {
+    width: 100%;
+    max-width: 281px;
+    border: 2px solid #07d0d3;
+    margin: 5px;
+    padding: 3px;
+  }
+
+  .caseclass {
+    text-align: center;
+    width: 100%;
+    color: #ffffff;
+    font-size: 20px;
+    font-weight: bold;
+    padding-top: 60px
+  }
+
+  .caseclass a {
+    color: #ffffff;
+    font-size: 18px;
+  }
+
+  .sitebottom {
+    padding: 5px;
+    text-align: center;
+  }
+
+  .sitewidth {
+    height: 203px;
+  }
+</style>
---
+++ src/views/node/marketingmodel/show.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <Modal
+      v-model="modal" width="900" :mask-closable="false"
+      :styles="{top: '20px'}">
+      <p slot="header">
+        <span>预览</span>
+      </p>
+      <div>
+        <Row style="">
+          <Col span="24">
+          <div style="width: 100%;margin: 0 auto;text-align: center;">
+            <div><p style="font-size:20px">{{form.title}}</p></div>
+          </div>
+          </Col>
+        </Row>
+        <Row style="border-bottom:1px solid #e3e8ee;margin:20px;padding-bottom: 10px">
+          <Col span="24">
+          <div style="width: 60%;margin: 0 auto;text-align: center">
+            <div>行业分类：{{form.industry_name}} &nbsp;&nbsp;  |关键词:{{form.keyword}}|&nbsp; &nbsp;  阅读：{{form.readcount}}</div>
+          </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col span="24">
+          <div v-html="form.content" style="overflow: hidden;font-size: 15px" class="img"> </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col span="24">
+          <div style="font-size: 20px;font-weight: 700;">核心解读:</div>
+          <div v-html="form.summary" style="overflow: hidden;font-size: 15px;" class="img"> </div>
+          </Col>
+        </Row>
+      </div>
+      <div slot="footer">
+        <Button type="success" size="large" :loading="modal_loading" @click="close">关闭</Button>
+      </div>
+    </Modal>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import http from '../../../libs/http';
+  export default {
+      data () {
+          return {
+              editorOption: {},
+              modal: false,
+              modal_loading: false,
+              form: {
+                  title: '',
+                  source: '',
+                  content: ''
+              }
+          };
+      },
+      methods: {
+          show (editid) {
+              this.apiGet('marketing_mode/' + editid).then((res) => {
+                  this.handleAjaxResponse(res, (data, msg) => {
+                      this.form = data;
+                      this.modal = true;
+                  }, (data, msg) => {
+                      this.$Message.error(msg);
+                  });
+              }, (res) => {
+                  // 处理错误信息
+                  this.$Message.error('网络异常，请稍后重试。');
+              });
+          },
+          close () {
+              this.modal = false;
+          }
+      },
+      mixins: [http]
+  };
+</script>
+<style scoped>
+  .img img{
+   max-width: 850px;
+  }
+
+</style>
