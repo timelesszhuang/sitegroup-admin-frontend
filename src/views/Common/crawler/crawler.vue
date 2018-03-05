a7073d596bb3cd899b30f93867bde4357c8e5a6f
yangjingzheng <1318911846@qq.com>
2018-03-05 10:50:39 +0800

    echarts

--- src/views/Common/crawler/crawler.vue
+++ src/views/Common/crawler/crawler.vue
@@ -1,143 +1,159 @@
 <template>
-    <div class="echarts">
-        <div style="width: 700px;height: 20px;margin: 0 auto">
-            <Row>
-                <Col span="9">
-                <Date-picker type="daterange" v-model="time" placement="bottom-end" placeholder="选择日期查询"></Date-picker>
-                </Col>
-                <Select v-model="site_id" style="width:310px;" label-in-value filterable clearable>
-                    <Option v-for="item in site" :value="item.id" :label="item.text" :key="item">
-                        {{ item.text }}
-                    </Option>
-                </Select>
-                &nbsp;<Button type="primary" @click="queryData">查询</Button>
-            </Row>
+
+    <card>
+        <div class="echarts">
+            <div style="width: 700px;height: 20px;margin: 0 auto">
+                <Row>
+                    <Col span="9">
+                    <Date-picker type="daterange" v-model="time" placement="bottom-end" placeholder="选择日期查询"></Date-picker>
+                    </Col>
+                    <Select v-model="site_id" style="width:310px;" label-in-value filterable clearable>
+                        <Option v-for="item in site" :value="item.id" :label="item.text" :key="item.id">
+                            {{ item.text }}
+                        </Option>
+                    </Select>
+                    &nbsp;<Button type="primary" @click="queryData">查询</Button>
+                </Row>
+            </div>
+            <div style=" width: 700px;
+    height: 500px;
+    margin: 0 auto;
+    padding-bottom: 50px;" id="spider-trend"></div>
         </div>
-        <IEcharts :option="bar" :loading="loading" @ready="onReady" @click="onClick"></IEcharts>
-    </div>
+    </card>
+
 </template>
-<script type="text/babel">
-    import echarts from 'echarts';
-    import http from '../../../libs/http';
 
-    export default {
-        name: 'view',
-        components: {
-            echarts,
-        },
-        props: {},
-        data: () => ({
-            site: [],
-            time: [],
-            site_id: "",
-            loading: false,
-            data: [],
-            bar: {
-                color: ["#20a0ff", "#13CE66", "#F7BA2A", "#FF4949", "#61a0a8", "#BBFFFF", "#FF1493", "#FF34B3", "#FF00FF", '#FFDAB9'],
-                title: {
-                    text: '爬虫统计',
-                    left: 'center',
-                    top: 10
-                },
-                tooltip: {
-                    trigger: 'axis'
-                },
-                legend: {
-                    bottom: 'bottom',
-                    data: []
-                },
-                grid: {
-                    left: '5%',
-                    right: '25%',
-                    bottom: '20%',
-                    containLabel: true
+<script>
+  import echarts from 'echarts';
+  import http from '../../../libs/http';
 
-                },
-
-                xAxis: {
-                    type: 'category',
-                    boundaryGap: false,
-                    data: [],
-                    axisLabel: {
-                        interval: 0,
-                        rotate: -25,
-                    },
-                    axisLine: {show: false},
-                    splitNumber: 100,
-                    splitLine: {
-                        show: true,
-                    },
-                },
-                yAxis: {
-                    type: 'value'
-                },
-                series: []
-            },
-
-        }),
-        methods: {
-            init() {
-                this.doRandom();
-                this.getSite((data) => {
-                    this.site = data
-                });
-            },
-            getSite() {
-                this.apiGet('Site/getSites').then((res) => {
-                    this.handleAjaxResponse(res, (data, msg) => {
-                        this.site = data
-//            console.log(this.site)
-                    }, (data, msg) => {
-                        this.$Message.error(msg);
-                    })
-                }, (res) => {
-                    //处理错误信息
-                    this.$Message.error('网络异常，请稍后重试。');
-                })
-            },
-            queryData() {
-                this.doRandom();
-                this.getSite((data) => {
-                    this.site = data
-                });
-            },
-            doRandom() {
-                const that = this;
-                let data = {
-                    params: {
-                        time: this.time,
-                        site_id: this.site_id,
-                    }
-                }
-                this.apiGet('count/enginecount', data).then((data) => {
-                    this.handleAjaxResponse(data, (data, msg) => {
-                        that.bar.legend.data = data.type;
-                        that.bar.xAxis.data = data.time;
-                        that.bar.series = data.type;
-                        that.bar.series[0].areaStyle = {normal: {}};
-                        that.bar.series[1].areaStyle = {normal: {}};
-                    }, (data, msg) => {
-                        this.$Message.error(msg);
-                    })
-                },)
-
-//        that.loading = !that.loading;
-            },
-            onReady(instance) {
-            },
-            onClick(event, instance, echarts) {
-            }
-        },
-        mixins: [http]
-
-    };
+  export default {
+      name: 'spiderTrend',
+      data () {
+          return {
+              site: [],
+              time: [],
+              site_id: '',
+              spiderTrendCharts: {},
+              xAxisData: [],
+              series: [],
+              color: this.$store.state.commondata.color
+          };
+      },
+      computed: {
+          finalseries: function () {
+              let length = this.color.length;
+              let _this = this;
+              let random = Math.ceil(Math.random() * 100);
+              return this.series.map(function (data, index) {
+                  let colorIndex = (index + random) % length;
+                  let color = _this.color[colorIndex];
+                  data['areaStyle'] = {
+                      normal: {
+                          color: color
+                      }
+                  };
+                  return data;
+              });
+              // 实例代码
+              return [
+                  {
+                      name: '运营商/网络服务',
+                      type: 'line',
+                      stack: '总量',
+                      areaStyle: {
+                          normal: {
+                              color: '#2d8cf0'
+                          }
+                      },
+                      data: [120, 132, 101, 134, 90, 230, 210]
+                  }
+              ];
+          },
+          option: function () {
+              return {
+                  tooltip: {
+                      trigger: 'axis',
+                      axisPointer: {
+                          type: 'cross',
+                          label: {
+                              backgroundColor: '#6a7985'
+                          }
+                      }
+                  },
+                  grid: {
+                      top: '3%',
+                      left: '1.2%',
+                      right: '1%',
+                      bottom: '3%',
+                      containLabel: true
+                  },
+                  xAxis: [
+                      {
+                          type: 'category',
+                          boundaryGap: false,
+                          data: this.xAxisData
+                      }
+                  ],
+                  yAxis: [
+                      {
+                          type: 'value'
+                      }
+                  ],
+                  series: this.finalseries
+              };
+          }
+      },
+      methods: {
+          queryData () {
+              this.getData();
+              this.getSite((data) => {
+                  this.site = data;
+              });
+          },
+          getSite () {
+              this.apiGet('getSites').then((res) => {
+                  this.handleAjaxResponse(res, (data, msg) => {
+                      this.site = data;
+                  }, (data, msg) => {
+                      this.$Message.error(msg);
+                  });
+              }, (res) => {
+                  // 处理错误信息
+                  this.$Message.error('网络异常，请稍后重试。');
+              });
+          },
+          getData () {
+              // 获取相关统计信息
+              let data = {
+                  params: {
+                      time: this.time,
+                      site_id: this.site_id
+                  }
+              };
+              this.apiGet('engineCount', data).then((res) => {
+                  this.handleAjaxResponse(res, (data, msg) => {
+                      this.series = data.type;
+                      this.xAxisData = data.time;
+                      this.spiderTrendCharts.setOption(this.option);
+                  });
+              });
+          },
+          init () {
+              this.spiderTrendCharts = echarts.init(document.getElementById('spider-trend'));
+              // 页面resize 的时候触发
+              let _this = this;
+              window.addEventListener('resize', function () {
+                  _this.spiderTrendCharts.resize();
+              });
+          }
+      },
+      mounted () {
+          this.init();
+          // 加载获取数据库中数据
+          this.getData();
+      },
+      mixins: [http]
+  };
 </script>
-
-<style scoped>
-    .echarts {
-        width: 800px;
-        height: 600px;
-        margin: 0 auto;
-        padding-bottom: 50px;
-    }
-</style>
--- src/views/node/pageview/index.vue
+++ src/views/node/pageview/index.vue
@@ -4,15 +4,15 @@
             <TabPane label="浏览量统计" name="pv">
                 <pv ref="pv"></pv>
             </TabPane>
-            <!--<TabPane label="浏览量展示" name="show">-->
-                <!--<show ref="show"></show>-->
-            <!--</TabPane>-->
-            <!--<TabPane label="爬虫统计" name="crawler">-->
-                <!--<crawler ref="crawler"></crawler>-->
-            <!--</TabPane>-->
-            <!--<TabPane label="搜索引擎关键词统计" name="countkeyword">-->
-                <!--<countkeyword ref="countkeyword"></countkeyword>-->
-            <!--</TabPane>-->
+            <TabPane label="浏览量展示" name="show">
+                <show ref="show"></show>
+            </TabPane>
+            <TabPane label="爬虫统计" name="crawler">
+                <crawler ref="crawler"></crawler>
+            </TabPane>
+            <TabPane label="搜索引擎关键词统计" name="countkeyword">
+                <countkeyword ref="countkeyword"></countkeyword>
+            </TabPane>
             <!--<TabPane label="其他统计" name="other">-->
                 <!--<other ref="other"></other>-->
             <!--</TabPane>-->
@@ -21,27 +21,26 @@
 </template>
 
 <script type="text/ecmascript-6">
-    import pv from '../../Common/pv/pv.vue'
-    // import show from '../../Common/show/index.vue'
-    // import crawler from '../../Common/crawler/crawler.vue'
-    // import countkeyword from '../../Common/countkeyword/index.vue'
+    import pv from '../../Common/pv/pv.vue';
+    import show from '../../Common/show/index.vue';
+    import crawler from '../../Common/crawler/crawler.vue';
+    import countkeyword from '../../Common/countkeyword/index.vue'
     // import other from '../../Common/other/index.vue'
 
     export default {
-        data() {
-            return {}
+        data () {
+            return {};
         },
         methods: {
-            tabClick(name) {
-                if (name !== "pv") {
+            tabClick (name) {
+                if (name !== 'pv') {
                     this.$refs[name].init();
                 }
             }
         },
         components: {
-            pv,
-          // show, crawler, countkeyword, other
+            pv, show, crawler, countkeyword
+            //  , other
         }
-    }
-
+    };
 </script>
