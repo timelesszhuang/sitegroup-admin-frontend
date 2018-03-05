65316c546aa4864101abb3a6b5519d43f7dc2b87
yangjingzheng <1318911846@qq.com>
2018-03-05 10:06:25 +0800

    营销模式 案例中心 总后台增删改查

--- src/views/node/pageview/index.vue
+++ src/views/node/pageview/index.vue
@@ -4,28 +4,28 @@
             <TabPane label="浏览量统计" name="pv">
                 <pv ref="pv"></pv>
             </TabPane>
-            <TabPane label="浏览量展示" name="show">
-                <show ref="show"></show>
-            </TabPane>
-            <TabPane label="爬虫统计" name="crawler">
-                <crawler ref="crawler"></crawler>
-            </TabPane>
-            <TabPane label="搜索引擎关键词统计" name="countkeyword">
-                <countkeyword ref="countkeyword"></countkeyword>
-            </TabPane>
-            <TabPane label="其他统计" name="other">
-                <other ref="other"></other>
-            </TabPane>
+            <!--<TabPane label="浏览量展示" name="show">-->
+                <!--<show ref="show"></show>-->
+            <!--</TabPane>-->
+            <!--<TabPane label="爬虫统计" name="crawler">-->
+                <!--<crawler ref="crawler"></crawler>-->
+            <!--</TabPane>-->
+            <!--<TabPane label="搜索引擎关键词统计" name="countkeyword">-->
+                <!--<countkeyword ref="countkeyword"></countkeyword>-->
+            <!--</TabPane>-->
+            <!--<TabPane label="其他统计" name="other">-->
+                <!--<other ref="other"></other>-->
+            <!--</TabPane>-->
         </Tabs>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
     import pv from '../../Common/pv/pv.vue'
-    import show from '../../Common/show/index.vue'
-    import crawler from '../../Common/crawler/crawler.vue'
-    import countkeyword from '../../Common/countkeyword/index.vue'
-    import other from '../../Common/other/index.vue'
+    // import show from '../../Common/show/index.vue'
+    // import crawler from '../../Common/crawler/crawler.vue'
+    // import countkeyword from '../../Common/countkeyword/index.vue'
+    // import other from '../../Common/other/index.vue'
 
     export default {
         data() {
@@ -39,7 +39,8 @@
             }
         },
         components: {
-            pv, show, crawler, countkeyword, other
+            pv,
+          // show, crawler, countkeyword, other
         }
     }
 
