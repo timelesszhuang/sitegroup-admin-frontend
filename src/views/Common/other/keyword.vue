694b8773c1150ada909d906d2f133ce50d5dd778
yangjingzheng <1318911846@qq.com>
2018-03-05 14:40:40 +0800

    count

--- src/views/Common/other/keyword.vue
+++ src/views/Common/other/keyword.vue
@@ -1,114 +1,11 @@
-<!--<template>-->
-  <!--<div class="echarts">-->
-    <!--<IEcharts :option="bar" :loading="loading" @ready="onReady" @click="onClick"></IEcharts>-->
-  <!--</div>-->
-<!--</template>-->
-<!--<script type="text/babel">-->
-  <!--import echarts from 'echarts';-->
-  <!--import http from '../../../libs/http';-->
-  <!--export default {-->
-    <!--name: 'view',-->
-    <!--components: {-->
-      <!--IEcharts,-->
-    <!--},-->
-    <!--props: {},-->
-    <!--data: () => ({-->
-      <!--loading: false,-->
-      <!--bar: {-->
-        <!--color: ["#13CE66","#F7BA2A","#FF4949"],-->
-        <!--tooltip: {-->
-          <!--trigger: 'axis',-->
-          <!--formatter: '{b} <br/>{a}:{c}个',-->
-          <!--axisPointer: {            // 坐标轴指示器，坐标轴触发有效-->
-            <!--type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'-->
-          <!--}-->
-        <!--},-->
-        <!--grid: {-->
-          <!--left: '20%',-->
-          <!--right: '25%',-->
-          <!--containLabel: true-->
-        <!--},-->
-        <!--title: {-->
-          <!--text: '关键词统计',-->
-          <!--left: 'center',-->
-          <!--top: 10-->
-        <!--},-->
-        <!--xAxis: [-->
-          <!--{-->
-            <!--formatter: '{b}<br/>{a}:{c}',-->
-            <!--type: 'category',-->
-            <!--data: [],-->
-            <!--axisLabel: {-->
-              <!--interval: 0,-->
-              <!--rotate: -20,-->
-              <!--formatter: function (value) {-->
-                <!--return value + "类关键词";-->
-              <!--}-->
-            <!--},-->
-          <!--}-->
-        <!--],-->
-        <!--yAxis: [-->
-          <!--{-->
-            <!--type: 'value'-->
-          <!--}-->
-        <!--],-->
-        <!--series: [-->
-          <!--{-->
-            <!--color: ["#13CE66","#F7BA2A","#FF4949"],-->
-            <!--name: '共',-->
-            <!--type: 'bar',-->
-            <!--barWidth: '17%',-->
-            <!--data: [],-->
-          <!--}-->
-        <!--]-->
-      <!--},-->
-
-    <!--}),-->
-    <!--methods: {-->
-      <!--init(){-->
-        <!--this.doRandom();-->
-      <!--},-->
-      <!--queryData() {-->
-        <!--this.doRandom();-->
-      <!--},-->
-      <!--doRandom() {-->
-        <!--const that = this;-->
-        <!--this.apiGet('keyword/KeywordCount').then((data) => {-->
-          <!--this.handleAjaxResponse(data, (data, msg) => {-->
-            <!--that.bar.series[0].data = data.count;-->
-            <!--that.bar.xAxis[0].data = data.name;-->
-          <!--}, (data, msg) => {-->
-            <!--this.$Message.error(msg);-->
-          <!--})-->
-        <!--},)-->
-
-<!--//        that.loading = !that.loading;-->
-      <!--},-->
-      <!--onReady(instance) {-->
-      <!--},-->
-      <!--onClick(event, instance, echarts) {-->
-      <!--}-->
-    <!--},-->
-    <!--mixins: [http]-->
-
-  <!--};-->
-<!--</script>-->
-
-<!--<style scoped>-->
-  <!--.echarts {-->
-    <!--width: 98%;-->
-   <!--margin-top: 30px;-->
-    <!--height: 500px;-->
-    <!--float: left;-->
-    <!--padding-bottom: 50px;-->
-  <!--}-->
-<!--</style>-->
 <template>
-  <div style=" width: 700px;
-    height: 500px;
-    margin: 0 auto;
-    padding-bottom: 50px;" id="pageview-keyword"></div>
+  <!--浏览量趋势-->
+  <div style="
 
+    height: 500px;
+
+    padding-bottom: 50px;
+  " id="keyword"></div>
 </template>
 
 <script>
@@ -116,12 +13,9 @@
   import http from '../../../libs/http';
 
   export default {
-      name: 'pageviewTrend',
+      name: 'spiderTr',
       data () {
           return {
-              site: [],
-              time: [],
-              site_id: '',
               spiderTrendCharts: {},
               xAxisData: [],
               series: [],
@@ -129,52 +23,38 @@
           };
       },
       computed: {
-          finalseries: function () {
-              let length = this.color.length;
-              let _this = this;
-              let random = Math.ceil(Math.random() * 100);
-              return this.series.map(function (data, index) {
-                  let colorIndex = (index + random) % length;
-                  let color = _this.color[colorIndex];
-                  data['areaStyle'] = {
-                      normal: {
-                          color: color
-                      }
-                  };
-                  data['name'] = '浏览量(pv)统计';
-                  return data;
-              });
-          },
           option: function () {
               return {
-                  title: {
-                      text: '浏览量统计',
-                      left: 'center',
-                      top: 10
-                  },
+                  color: ['#3398DB'],
                   tooltip: {
-                      trigger: 'axis',
                       formatter: '{b} <br/>{a}:{c}个',
+                      trigger: 'axis',
                       axisPointer: { // 坐标轴指示器，坐标轴触发有效
                           type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
                       }
                   },
+                  title: {
+                      text: '关键词统计',
+                      left: 'center',
+                      top: 10
+                  },
                   grid: {
                       left: '20%',
-                      right: '25%',
+                      right: '20%',
+                      bottom: '3%',
                       containLabel: true
                   },
                   xAxis: [
                       {
-                          formatter: '{b}<br/>{a}:{c}',
                           type: 'category',
-                          data: [],
+                          data: this.xAxisData,
                           axisLabel: {
-                              interval: 0,
-                              rotate: -20,
                               formatter: function (value) {
                                   return value + '类关键词';
                               }
+                          },
+                          axisTick: {
+                              alignWithLabel: true
                           }
                       }
                   ],
@@ -183,47 +63,31 @@
                           type: 'value'
                       }
                   ],
-                  series: this.finalseries
+                  series: [
+                      {
+                          name: '共',
+                          type: 'bar',
+                          barWidth: '17%',
+                          data: this.series
+                      }
+                  ]
+
               };
           }
       },
       methods: {
-          queryData () {
-              this.getData();
-              this.getSite((data) => {
-                  this.site = data;
-              });
-          },
-          getSite () {
-              this.apiGet('getSites').then((res) => {
-                  this.handleAjaxResponse(res, (data, msg) => {
-                      this.site = data;
-                  }, (data, msg) => {
-                      this.$Message.error(msg);
-                  });
-              }, (res) => {
-                  // 处理错误信息
-                  this.$Message.error('网络异常，请稍后重试。');
-              });
-          },
           getData () {
               // 获取相关统计信息
-              let data = {
-                  params: {
-                      time: this.time,
-                      site_id: this.site_id
-                  }
-              };
-              this.apiGet('pvStatistic', data).then((res) => {
+              this.apiGet('keyword_count').then((res) => {
                   this.handleAjaxResponse(res, (data, msg) => {
-                      this.series = data.type;
-                      this.xAxisData = data.time;
+                      this.series = data.count;
+                      this.xAxisData = data.name;
                       this.spiderTrendCharts.setOption(this.option);
                   });
               });
           },
           init () {
-              this.spiderTrendCharts = echarts.init(document.getElementById('pageview-keyword'));
+              this.spiderTrendCharts = echarts.init(document.getElementById('keyword'));
               // 页面resize 的时候触发
               let _this = this;
               window.addEventListener('resize', function () {
@@ -232,9 +96,6 @@
           }
       },
       mounted () {
-          this.getSite((data) => {
-              this.site = data;
-          });
           this.init();
           // 加载获取数据库中数据
           this.getData();
@@ -242,11 +103,3 @@
       mixins: [http]
   };
 </script>
-<style scoped>
-  .echarts {
-    width: 800px;
-    height: auto;
-    margin: 0 auto;
-    padding-bottom: 50px;
-  }
-</style>
--- src/views/Common/other/questioncount.vue
+++ src/views/Common/other/questioncount.vue
@@ -1,103 +1,105 @@
 <template>
-  <div class="echarts">
-    <IEcharts :option="bar" :loading="loading" @ready="onReady" @click="onClick"></IEcharts>
-  </div>
-</template>
-<script type="text/babel">
-  import echarts from 'echarts';
-  import http from '../../../libs/http';
-  export default {
-    name: 'view',
-    components: {
-      IEcharts,
-    },
-    props: {},
-    data: () => ({
-      loading: false,
-      bar: {
-        color: ["#BBFFFF"],
-        tooltip: {
-          trigger: 'axis',
-          formatter: '{b} <br/>{a}:{c}篇',
-          axisPointer: {            // 坐标轴指示器，坐标轴触发有效
-            type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-          }
-        },
-        grid: {
-          left: '20%',
-          right: '25%',
-          containLabel: true
-
-        },
-        title: {
-          text: '问答统计',
-          left: 'center',
-          top: 10
-        },
-        xAxis: [
-          {
-            type: 'category',
-            data: [],
-            axisLabel: {
-              interval: 0,
-              rotate: -20,
-
-
-            }
-          }
-        ],
-        yAxis: [
-          {
-            type: 'value'
-          }
-        ],
-        series: [
-          {
-            name: '共',
-            type: 'bar',
-            barWidth: '17%',
-            data: [],
-          }
-        ]
-      },
-
-    }),
-    methods: {
-      init() {
-        this.doRandom();
-      },
-      queryData() {
-        this.doRandom();
-      },
-      doRandom() {
-        const that = this;
-        this.apiGet('questiontype/QuestionCount').then((data) => {
-          this.handleAjaxResponse(data, (data, msg) => {
-            that.bar.series[0].data = data.count;
-            that.bar.xAxis[0].data = data.name;
-          }, (data, msg) => {
-            this.$Message.error(msg);
-          })
-        },)
-
-//        that.loading = !that.loading;
-      },
-      onReady(instance) {
-      },
-      onClick(event, instance, echarts) {
-      }
-    },
-    mixins: [http]
-
-  };
-</script>
-
-<style scoped>
-  .echarts {
-    width: 98%;
-
+  <!--浏览量趋势-->
+  <div style="
+    width:100%;
     height: 500px;
     float: left;
     padding-bottom: 50px;
-  }
-</style>
+  " id="question_count"></div>
+</template>
+
+<script>
+  import echarts from 'echarts';
+  import http from '../../../libs/http';
+
+  export default {
+      name: 'spiderTr',
+      data () {
+          return {
+              spiderTrendCharts: {},
+              xAxisData: [],
+              series: [],
+              color: this.$store.state.commondata.color
+          };
+      },
+      computed: {
+          option: function () {
+              return {
+                  tooltip: {
+                      formatter: '{b} <br/>{a}:{c}篇',
+                      trigger: 'axis',
+                      axisPointer: {
+                          type: 'cross',
+                          label: {
+                              backgroundColor: '#6a7985'
+                          }
+                      }
+                  },
+                  title: {
+                      text: '问答统计',
+                      left: 'center',
+                      top: 10
+                  },
+                  grid: {
+                      top: '15%',
+                      left: '1%',
+                      right: '2%',
+                      bottom: '15%',
+                      containLabel: true
+                  },
+                  xAxis: [
+                      {
+                          type: 'category',
+                          boundaryGap: false,
+                          data: this.xAxisData,
+                          axisLabel: {
+                              interval: 0,
+                              rotate: -33
+                          }
+                      }
+                  ],
+                  yAxis: [
+                      {
+                          type: 'value'
+                      }
+                  ],
+                  series: [
+                      {
+                          name: '共',
+                          type: 'bar',
+                          barWidth: '17%',
+                          data: this.series
+                      }
+                  ]
+
+              };
+          }
+      },
+      methods: {
+          getData () {
+              // 获取相关统计信息
+              this.apiGet('questionCount').then((res) => {
+                  this.handleAjaxResponse(res, (data, msg) => {
+                      this.series = data.count;
+                      this.xAxisData = data.name;
+                      this.spiderTrendCharts.setOption(this.option);
+                  });
+              });
+          },
+          init () {
+              this.spiderTrendCharts = echarts.init(document.getElementById('question_count'));
+              // 页面resize 的时候触发
+              let _this = this;
+              window.addEventListener('resize', function () {
+                  _this.spiderTrendCharts.resize();
+              });
+          }
+      },
+      mounted () {
+          this.init();
+          // 加载获取数据库中数据
+          this.getData();
+      },
+      mixins: [http]
+  };
+</script>
